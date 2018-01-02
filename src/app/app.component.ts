3dc2fbe0728d2914a178f9c17d45d79d5a7a6d9b
shj7972 <seo.hyunjong@gmail.com>
2018-01-02 22:10:15 +0900

    simpleLogin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,24 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import { LoginPage} from '../pages/login/login';
+
+import * as firebase from "firebase";
+
+var config = {
+  apiKey: "AIzaSyBuU4OPNZu9EwABdbC_e94SJZOQG7DTIA4",
+  authDomain: "simplelogin-4ea16.firebaseapp.com",
+  databaseURL: "https://simplelogin-4ea16.firebaseio.com",
+  projectId: "simplelogin-4ea16",
+  storageBucket: "simplelogin-4ea16.appspot.com",
+  messagingSenderId: "476072182813"
+};
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage:any;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
@@ -16,6 +29,15 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+    });
+    
+    firebase.initializeApp(config);
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        this.rootPage = HomePage;
+      } else {
+        this.rootPage = LoginPage;
+      }
     });
   }
 }
